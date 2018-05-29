252eff51e8f0b39a18d8149734dc2164629eef8a
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-05-29 14:36:53 +0100

    Create new test file

---
+++ selenium/GettingStarted/DemoWebApp2.Tests/LoanApplicationTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoWebApp2.Tests
+{
+    class LoanApplicationTests
+    {
+    }
+}
